0f6631df29fb67be8322e23211ac048e80249d83
Jesse <jesseolatunjioluwadare@gmail.com>
2022-08-31 10:45:19 +0100

    HTTP module - intro

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write(`Welcome to our page`);
+  res.end();
+});
+
+server.listen(5000, () => {
+  console.log(`...server fm`);
+});
