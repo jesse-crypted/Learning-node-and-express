2fb625977b7d1f1d9c41cd7aec5cf5391dd4a66f
Jesse <jesseolatunjioluwadare@gmail.com>
2022-08-31 10:59:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write(`Welcome to our page`);
-  res.end();
+  if (req.url === "/") {
+    res.end(`Welcome to Home page`);
+  } else if (req.url === "/about") {
+    res.end(`Welcome to About page`);
+  } else {
+    res.end(`<h1>Oops 404 Error</h1>
+  <p>We cant seem to get that page. </p>
+  <a href="/">back home. </a>`);
+  }
 });
 
 server.listen(5000, () => {
